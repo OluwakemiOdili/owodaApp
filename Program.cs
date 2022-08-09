c8ed23fca44c46fdfc2c1095d82e44096de98816
Support <Support@WEMA-TRAINING-S>
2022-08-09 15:10:37 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace owodaApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Owoda");
+            Console.WriteLine();
+
+            Owoda app = new Owoda();
+            Admin admin = new Admin();
+
+            app.PricePerTrip();
+
+            Console.WriteLine("The below calculate for 10 Random Drivers");
+            Console.WriteLine();
+            admin.TotalAmountForTenDrivers();
+        }
+    }
+}
